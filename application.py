1e8bae44192c8452230f741f607a1e2f175f3d35
hananina <hananinacode@gmail.com>
2016-06-27 12:16:15 -0400

    function 5 - delete

--- application.py
+++ application.py
@@ -23,23 +23,23 @@ def home():
 def showCategoryItems(category_slug):
     theCategory = session.query(Category).filter(Category.slug == category_slug).one()
     items  = session.query(Item).filter_by(category_id = theCategory.id)
-    return render_template('categoryitems.html', category_name=theCategory.name, items=items)
+    return render_template('categoryitems.html', category_slug=category_slug, category_name=theCategory.name, items=items)
 
 
 @app.route('/<string:category_slug>/<string:item_slug>')
 def showItem(category_slug, item_slug):
     theCategory = session.query(Category).filter(Category.slug == category_slug).one()
     item = session.query(Item).filter(Item.slug == item_slug).one()
-    return render_template('item.html', category_name=theCategory.name, item=item)
+    return render_template('item.html', category_slug=category_slug, category_name=theCategory.name, item=item)
 
 
 @app.route('/<string:category_slug>/<string:item_slug>/edit', methods = ['GET','POST'])
 def editItem(category_slug, item_slug):
     item = session.query(Item).filter(Item.slug == item_slug).one()
-    
-    if request.method == "POST":
 
+    if request.method == "POST":
         item.name = request.form['name']
+        item.slug = request.form['name'].lower().replace(' ','_')
         item.description = request.form['description']
         item.category_id = request.form['category_id']
         session.add(item)
@@ -48,7 +48,6 @@ def editItem(category_slug, item_slug):
 
         # to get category slug from updated item, in order to use redirect argument
         theCategory = session.query(Category).filter(Category.id == item.category_id).one()
-
         return redirect (url_for('showItem', category_slug=theCategory.slug, item_slug= item.slug))      
 
     else:
@@ -56,14 +55,25 @@ def editItem(category_slug, item_slug):
         theCategory = session.query(Category).filter(Category.slug == category_slug).one()
         # to get categories for edit category the item is belonged.
         categories = session.query(Category).all()
-        return render_template('edititem.html', item=item, theCategory=theCategory, categories=categories)
+        return render_template('edititem.html', item=item, category_slug=category_slug, category_name=theCategory.name, categories=categories)
 
 
-@app.route('/<string:item_slug>/delete')
-def deleteItem():
-    return render_template('deleteitem.html')
+@app.route('/<string:category_slug>/<string:item_slug>/delete', methods = ['GET','POST'])
+def deleteItem(category_slug, item_slug):
+    item = session.query(Item).filter(Item.slug == item_slug).one()
+    theCategory = session.query(Category).filter(Category.slug == category_slug).one()
+    
+    if request.method == "POST":
+        session.delete(item)
+        session.commit()
+        flash(item.name + "has been deleted succesfully!")
+        return redirect(url_for('showCategoryItems', category_slug=category_slug))
 
-@app.route('/<string:item_slug>/add')
+    else:
+        return render_template('deleteitem.html', item=item, category_slug=category_slug, category_name= theCategory.name)
+
+
+@app.route('/<string:category_slug>/<string:item_slug>/add')
 def addItem():
     return render_template('additem.html')
 

5d4a1caeb9ec9be754e7f9605e910b78daee64ab
hananina <hananinacode@gmail.com>
2016-07-26 01:34:17 -0400

    fb connect error

--- application.py
+++ application.py
@@ -51,6 +51,71 @@ def showLogin():
     return render_template('login.html', STATE = state)
 
 
+@app.route('/fbconnect', methods=['POST'])
+def fbconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = request.data
+    print "access token received %s " % access_token
+
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
+        'web']['app_id']
+    app_secret = json.loads(
+        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+        app_id, app_secret, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+    # Use token to get user info from API
+    userinfo_url = "https://graph.facebook.com/v2.4/me"
+    # strip expire tag from access token
+    token = result.split("&")[0]
+
+
+    url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    # print "url sent for API access:%s"% url
+    # print "API JSON result: %s" % result
+    data = json.loads(result)
+    login_session['provider'] = 'facebook'
+    login_session['username'] = data["name"]
+    login_session['email'] = data["email"]
+    login_session['facebook_id'] = data["id"]
+
+    # The token must be stored in the login_session in order to properly logout, let's strip out the information before the equals sign in our token
+    stored_token = token.split("=")[1]
+    login_session['access_token'] = stored_token
+
+    # Get user picture
+    url = 'https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&height=200&width=200' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+
+    login_session['picture'] = data["data"]["url"]
+
+    # see if user exists
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+
+    flash("Now logged in as %s" % login_session['username'])
+    return output
+
 # to generate unique session token
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
@@ -134,7 +199,6 @@ def gconnect():
         user_id = createUser(login_session)
 
     login_session['user_id'] = user_id
-
 
     output = ''
     output += '<h1>Welcome, '
@@ -242,11 +306,15 @@ def showItem(category_slug, item_slug):
 
 @app.route('/<string:category_slug>/<string:item_slug>/edit', methods = ['GET','POST'])
 def editItem(category_slug, item_slug):
+    
     # to check the user is logged in.
     if 'username' not in login_session:
         return redirect('/login')
 
     item = session.query(Item).filter(Item.slug == item_slug).one()
+
+    if item.user_id != login_session['user_id']:
+        return "<script>function alertFunction(){ alert('You are not authrized to delete this item.');}</script><body onload='alertFunction()'>"
 
     if request.method == "POST":
         item.user_id = login_session['user_id']
@@ -280,7 +348,6 @@ def deleteItem(category_slug, item_slug):
     theCategory = session.query(Category).filter(Category.slug == category_slug).one()
     
     if item.user_id != login_session['user_id']:
-        print "n000!!!!!!"
         return "<script>function alertFunction(){ alert('You are not authrized to delete this item.');}</script><body onload='alertFunction()'>"
 
     if request.method == "POST":
